8d3afa21487b1ff59e8c5f6c44d6927c2a04146b
Parshuram Patil <parshuram.patil@outlook.in>
2023-10-23 15:49:53 +0530

    add public subnet

--- tf/main.tf
+++ tf/main.tf
@@ -4,3 +4,17 @@ resource "aws_vpc" "WebServerVPC" {
     Name = "WebServerVPC"
   })
 }
+
+data "aws_availability_zones" "AvailableZones" {
+  state = "available"
+}
+
+resource "aws_subnet" "WebServerPublicSubnet" {
+  vpc_id     = aws_vpc.WebServerVPC.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = data.aws_availability_zones.AvailableZones.names[0]
+  map_public_ip_on_launch = true
+  tags =  merge(local.common_tags, {
+    Name = "WebServerPublicSubnet"
+  })
+}
