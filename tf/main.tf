4ada91445c64377416a6d96598ef1b37a21fb776
Parshuram Patil <parshuram.patil@outlook.in>
2023-11-14 13:13:56 +0530

    add missing egress config

--- tf/main.tf
+++ tf/main.tf
@@ -80,4 +80,11 @@ resource "aws_security_group" "HttpOnlySecurityGroup" {
     protocol    = "tcp"
     cidr_blocks = [var.AnyIpCidrBlock]
   }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
